2bc9562dfa27b507e29543d821f6381e322215d6
zhaohangzhen <13543320057@126.com>
2024-09-06 21:46:22 +0800

    Update 帝国战争.py

--- 帝国战争.py
+++ 帝国战争.py
@@ -3,7 +3,10 @@ import streamlit as st
 page = st.sidebar.radio('帝国战争',['注册', '开始游戏'])
 
 def start_html():
-  pass
+  st.image('微信图片_20240606215133.jpg')
+  go = st.selectbox('编程猫社区', ['进入编程猫社区实现幻想工作室'])
+  if go == '进入编程猫社区':
+    st.link_button('', 'https://shequ.codemao.cn/work_shop/24992')
 
 def play_game():
   pass
